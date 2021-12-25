20b6501fca1b5ad8753fc508dbb18172de48f9e3
Dior <Tum343619!>
2021-12-25 10:14:37 +0100

    leetcode problem 704

---
+++ problems/Solution704.java
@@ -0,0 +1,26 @@
+package problems;
+
+/**
+ * Problem: https://leetcode.com/problems/binary-search/
+ * Time Complexity: O(logN)
+ * Space Complexity： O(1)
+ */
+class Solution704 {
+    public int search(int[] nums, int target) {
+        int l = -1, r = nums.length;
+        int m;
+        while (l < r - 1) {
+            m = (l + r) / 2;
+            if (nums[m] < target) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        if (r!= nums.length && nums[r] == target) {
+            return r;
+        } else {
+            return -1;
+        }
+    }
+}
