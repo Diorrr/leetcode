df6fd879c93e67b2a8a89fac37cd1a3ca24b86cf
Dior <Tum343619!>
2021-12-25 09:02:40 +0100

    leetcode problem 268

---
+++ problems/Solution268.java
@@ -0,0 +1,26 @@
+package problems;
+
+import java.util.Arrays;
+
+/**
+ * Problem: https://leetcode.com/problems/missing-number/
+ * Time Complexity: O(N*logN)
+ * Space Complexity：
+ */
+class Solution268 {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        if (nums[0] != 0) {
+            return 0;
+        } else if (nums[nums.length - 1] == nums.length - 1) {
+            return nums.length;
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != i) {
+                    return i;
+                }
+            }
+        }
+        return 0;
+    }
+}
