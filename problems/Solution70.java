54f5b97d78b204775ac6cb053eef3f34731c1057
Dior <Tum343619!>
2021-12-26 08:26:23 +0100

    leetcode problem 70.2

--- problems/Solution70.java
+++ problems/Solution70.java
@@ -7,13 +7,31 @@ package problems;
  */
 class Solution70 {
     public int climbStairs(int n) {
-        int[] array = new int[46];
-        array[1] = 1;
-        array[2] = 2;
-        array[3] = 3;
+        int[]array = new int[46];
+        array[1]=1;
+        array[2]=2;
+        array[3]=3;
         for (int i = 4; i <= n; i++) {
-            array[i] = array[i - 1] + array[i - 2];
+            array[i]=array[i-1]+array[i-2];
         }
         return array[n];
+    }
+
+    // solution without array with 3 additional variables
+    public int climbStairs2(int n) {
+        int stepOne = 1;
+        int stepTwo = 2;
+        int stepN = 0;
+        if (n == 1) {
+            return stepOne;
+        } else if (n == 2) {
+            return stepTwo;
+        }
+        for (int i = 3; i <= n; i++) {
+            stepN = stepOne + stepTwo;
+            stepOne = stepTwo;
+            stepTwo = stepN;
+        }
+        return stepN;
     }
 }
