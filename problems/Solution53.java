ca46c2118990ee152e246867809f197c316a2632
Dior <rustamovdior@gmail.com>
2021-12-27 07:13:30 +0100

    leetcode problem 53

---
+++ problems/Solution53.java
@@ -0,0 +1,18 @@
+package problems;
+
+/**
+ * Problem: https://leetcode.com/problems/maximum-subarray/
+ * Time Complexity: O(N)
+ * Space Complexity：O(1)
+ */
+class Solution53 {
+    public int maxSubArray(int[] nums) {
+        int sum = 0, maxSum = nums[0], minSum = 0;
+        for (int num : nums) {
+            sum += num;
+            maxSum = Math.max(sum - minSum, maxSum);
+            minSum = Math.min(minSum, sum);
+        }
+        return Math.max(maxSum, nums[nums.length - 1]);
+    }
+}
