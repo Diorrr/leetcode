4f70828360212ed5cfe9e3dc250115c50cfe2c0b
Dior <rustamovdior@gmail.com>
2021-12-29 19:23:38 +0100

    leetcode problem 141

---
+++ problems/easy/Solution141.java
@@ -0,0 +1,50 @@
+package problems.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Problem:
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution141 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        while (head.next != null) {
+            if (head.val == Integer.MAX_VALUE) {
+                return true;
+            }
+            head.val = Integer.MAX_VALUE;
+            head = head.next;
+        }
+        return false;
+    }
+
+    public boolean hasCycle2(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        Set<Integer> set = new HashSet<>();
+        while (head.next != null) {
+            if (set.contains(head.hashCode())) {
+                return true;
+            }
+            set.add(head.hashCode());
+            head = head.next;
+        }
+        return false;
+    }
+}
