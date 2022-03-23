099a1b40f1f5d3c98bcbb09ac63ab73632a0b989
Dior <rustamovdior@gmail.com>
2022-03-23 13:09:37 +0100

    leetcode problem 130,617

---
+++ problems/easy/Solution617.java
@@ -0,0 +1,66 @@
+package problems.easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Problem: https://leetcode.com/problems/merge-two-binary-trees/
+ * Time Complexity: O(m+n)
+ * Space Complexity：
+ */
+class Solution617 {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null || root2 == null) {
+            return root1 != null ? root1 : root2;
+        }
+
+        Queue<TreeNode> queue1 = new LinkedList<>();
+        Queue<TreeNode> queue2 = new LinkedList<>();
+        queue1.add(root1);
+        queue2.add(root2);
+        while (!queue1.isEmpty() || !queue2.isEmpty()) {
+            TreeNode treeNode1 = queue1.remove();
+            TreeNode treeNode2 = queue2.remove();
+
+            if (treeNode1 != null && treeNode2 != null) {
+                treeNode1.val += treeNode2.val;
+            }
+
+            if (treeNode1.left != null && treeNode2.left != null) {
+                queue1.add(treeNode1.left);
+                queue2.add(treeNode2.left);
+            } else if (treeNode1.left == null) {
+                treeNode1.left = treeNode2.left;
+            }
+
+            if (treeNode1.right != null && treeNode2.right != null) {
+                queue1.add(treeNode1.right);
+                queue2.add(treeNode2.right);
+            } else if (treeNode1.right == null) {
+                treeNode1.right = treeNode2.right;
+            }
+
+
+        }
+        return root1;
+    }
+}
