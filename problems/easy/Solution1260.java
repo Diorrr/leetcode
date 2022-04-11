6e79a4a68b0bfd4d491e1929ba762684b4028c2f
Dior <rustamovdior@gmail.com>
2022-04-11 19:15:50 +0200

    leetcode problem 120

---
+++ problems/easy/Solution1260.java
@@ -0,0 +1,36 @@
+package problems.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Problem: https://leetcode.com/problems/shift-2d-grid/
+ * Time Complexity: O(mn)
+ * Space Complexity： O(mn)
+ */
+class Solution1260 {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        for (int[] ints : grid) {
+            for (int anInt : ints) {
+                temp.add(anInt);
+            }
+        }
+        k = k % temp.size();
+        int pos = temp.size()-k;
+        for (int[] ints : grid) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < ints.length; j++) {
+                if (pos == temp.size()) {
+                    pos = 0;
+                }
+                row.add(temp.get(pos));
+                pos++;
+            }
+            res.add(row);
+        }
+
+        return res;
+    }
+}
