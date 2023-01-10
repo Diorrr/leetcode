9e9ad1352e2dc455d9cecd87c7c8735ceefc7879
Dior <rustamovdior@gmail.com>
2023-01-10 23:38:35 +0100

    leetcode problem 40,216

--- problems/medium/Solution40.java
+++ problems/medium/Solution40.java
@@ -11,6 +11,7 @@ import java.util.List;
  */
 class Solution40 {
     List<List<Integer>> res = new ArrayList<>();
+
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         backtrack(candidates, target, 0, new ArrayList<Integer>());
@@ -18,10 +19,11 @@ class Solution40 {
     }
 
     private void backtrack(int[] candidates, int target, int index, List<Integer> list) {
-        if (target == 0 && !res.stream().anyMatch(t->t.equals(list))) {
+        if (target == 0) {
             res.add(list);
         } else if (target > 0) {
             for (int i = index; i < candidates.length; i++) {
+                if (i > index && candidates[i] == candidates[i - 1]) continue;
                 List<Integer> newList = new ArrayList<>(list);
                 newList.add(candidates[i]);
                 backtrack(candidates, target - candidates[i], i + 1, newList);
