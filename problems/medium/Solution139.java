e15ddccf3e6114f05a1472c3a4b7c4025b41d6e9
Dior <rustamovdior@gmail.com>
2023-01-17 08:26:45 +0100

    leetcode problem 139

---
+++ problems/medium/Solution139.java
@@ -0,0 +1,31 @@
+package problems.medium;
+
+import java.util.List;
+
+/**
+ * Problem: https://leetcode.com/problems/word-break/description/
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        for (String word : wordDict) {
+            if (s.startsWith(word)) {
+                dp[word.length()] = true;
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (dp[i]) {
+                for (String word : wordDict) {
+                    if (s.startsWith(word, i)) {
+                        dp[i + word.length()] = true;
+                    }
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
