3e0a12b693db79c077fd7567f3e0bb534422745b
Dior <rustamovdior@gmail.com>
2023-02-20 00:09:57 +0100

    leetcode problem 215

---
+++ problems/medium/priorityqueue/Solution215.java
@@ -0,0 +1,23 @@
+package problems.medium.priorityqueue;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+/**
+ * Problem:
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution215 {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int num : nums) {
+            queue.add(num);
+        }
+        for (int i = 0; i < k - 1; i++) {
+            queue.poll();
+        }
+
+        return queue.poll();
+    }
+}
