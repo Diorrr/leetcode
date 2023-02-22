c7d53f74dac40e48882ca837e07c1eeddadb282d
Dior <rustamovdior@gmail.com>
2023-02-22 10:55:11 +0100

    leetcode problem 852

---
+++ problems/medium/binsearch/Solution852.java
@@ -0,0 +1,24 @@
+package problems.medium.binsearch;
+
+/**
+ * Problem: https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution852 {
+    public int peakIndexInMountainArray(int[] arr) {
+        int index = 0;
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (arr[m - 1] < arr[m] && arr[m] > arr[m + 1]) {
+                return m;
+            } else if (arr[m - 1] < arr[m]) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        return index;
+    }
+}
