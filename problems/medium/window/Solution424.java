da81aaac6be745baa441d2a7792d61c62d785424
Dior <rustamovdior@gmail.com>
2023-02-25 22:24:59 +0100

    leetcode problem 424

---
+++ problems/medium/window/Solution424.java
@@ -0,0 +1,23 @@
+package problems.medium.window;
+
+/**
+ * Problem: https://leetcode.com/problems/longest-repeating-character-replacement/description/
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution424 {
+    public int characterReplacement(String s, int k) {
+        int maxOcc = 0, start = 0, res = 0;
+        int[] count = new int[26];
+
+        for (int end = 0; end < s.length(); end++) {
+            maxOcc = Math.max(maxOcc, ++count[s.charAt(end) - 'A']);
+            while (end - start + 1 > maxOcc + k) {
+                count[s.charAt(start) - 'A']--;
+                start++;
+            }
+            res = Math.max(res, end - start + 1);
+        }
+        return res;
+    }
+}
