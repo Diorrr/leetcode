67a251c2934ae1361e45fbaaefd7eb6f423d38ca
Dior <rustamovdior@gmail.com>
2023-03-10 17:07:09 +0100

    leetcode problem 208

---
+++ problems/medium/string/Solution208.java
@@ -0,0 +1,82 @@
+package problems.medium.string;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution208 {
+    /**
+     * Problem: https://leetcode.com/problems/implement-trie-prefix-tree/
+     * Time Complexity:
+     * Space Complexity：
+     */
+
+    class Node {
+        public boolean isWord = false;
+        public Node[] children = new Node[26];
+
+        public Node() {
+        }
+    }
+
+    class Trie {
+        Node root;
+
+        public Trie() {
+            root = new Node();
+        }
+
+        public void insert(String word) {
+            Node curr = root;
+            for (int i = 0; i < word.length(); i++) {
+                if (curr.children[word.charAt(i) - 'a'] == null) {
+                    curr.children[word.charAt(i) - 'a'] = new Node();
+                }
+                curr = curr.children[word.charAt(i) - 'a'];
+            }
+            curr.isWord = true;
+        }
+
+        public boolean search(String word) {
+            Node curr = root;
+            for (int i = 0; curr != null && i < word.length(); i++) {
+                curr = curr.children[word.charAt(i) - 'a'];
+            }
+            return curr != null && curr.isWord;
+        }
+
+        public boolean startsWith(String prefix) {
+            Node curr = root;
+            for (int i = 0; curr != null && i < prefix.length(); i++) {
+                curr = curr.children[prefix.charAt(i) - 'a'];
+            }
+
+            return curr != null;
+        }
+    }
+
+    class Trie2 {
+        Set<String> prefixSet;
+
+        Set<String> words;
+
+        public Trie2() {
+            prefixSet = new HashSet<>();
+            words = new HashSet<>();
+        }
+
+        public void insert(String word) {
+            words.add(word);
+            for (int i = 1; i < word.length() + 1; i++) {
+                prefixSet.add(word.substring(0, i));
+            }
+        }
+
+        public boolean search(String word) {
+            return words.contains(word);
+        }
+
+        public boolean startsWith(String prefix) {
+            return prefixSet.contains(prefix);
+        }
+    }
+}
