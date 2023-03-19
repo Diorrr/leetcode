f501285e2c7678c3f2a3aee6e97b93d29500950c
Dior <rustamovdior@gmail.com>
2023-03-19 22:08:57 +0100

    leetcode problem 117

---
+++ problems/medium/bfs/Solution117.java
@@ -0,0 +1,58 @@
+package problems.medium.bfs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Problem: https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/description/
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution117 {
+    public Node connect(Node root) {
+        if (root != null) {
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(root);
+
+            while (!queue.isEmpty()) {
+                List<Node> level = new ArrayList<>();
+                while (!queue.isEmpty()) {
+                    Node curr = queue.poll();
+                    curr.next = queue.peek();
+                    if (curr.left != null) {
+                        level.add(curr.left);
+                    }
+                    if (curr.right != null) {
+                        level.add(curr.right);
+                    }
+                }
+                queue.addAll(level);
+            }
+        }
+
+        return root;
+    }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+}
