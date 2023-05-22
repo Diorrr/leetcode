a64674d5e23115bb5a16638c9c597f1e5386e31c
Dior <rustamovdior@gmail.com>
2023-05-22 20:42:42 +0200

    leetcode problem 1706

---
+++ problems/medium/arrays/Solution1706.java
@@ -0,0 +1,45 @@
+package problems.medium.arrays;
+
+/**
+ * Problem: https://leetcode.com/problems/where-will-the-ball-fall/
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution1706 {
+    public int[] findBall(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[] answer = new int[n];
+        for (int idx = 0; idx < answer.length; idx++) {
+            int x = 0, y = idx;
+            while (answer[idx] == 0) {
+                if (grid[x][y] == 1) {
+                    if (y + 1 >= n) {
+                        answer[idx] = -1;
+                    } else if (grid[x][y + 1] == -1) {
+                        answer[idx] = -1;
+                    } else if (x + 1 >= m) {
+                        answer[idx] = y + 1;
+                        break;
+                    } else {
+                        x++;
+                        y++;
+                    }
+                } else {
+                    if (y == 0) {
+                        answer[idx] = -1;
+                    } else if (grid[x][y - 1] == 1) {
+                        answer[idx] = -1;
+                    } else if (x + 1 >= m) {
+                        answer[idx] = y - 1;
+                        break;
+                    } else {
+                        x++;
+                        y--;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
