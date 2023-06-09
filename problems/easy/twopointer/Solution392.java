7afe511d1aa5ec765a6483a78340b0a78a032ca4
Dior <rustamovdior@gmail.com>
2023-06-09 18:01:25 +0200

    leetcode problem 392

---
+++ problems/easy/twopointer/Solution392.java
@@ -0,0 +1,57 @@
+package problems.easy.twopointer;
+
+/**
+ * Problem: <a href="https://leetcode.com/problems/is-subsequence">
+ * is-subsequence</a>
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution392 {
+    public boolean isSubsequence(String s, String t) {
+        int pointerS = 0;
+        int pointerT = 0;
+        int lenghtS = s.length();
+        int lenghtT = t.length();
+        char sCharArray[] = s.toCharArray();
+        char tCharArray[] = t.toCharArray();
+
+        while (pointerT < lenghtT && pointerS < lenghtS) {
+            if (sCharArray[pointerS] == tCharArray[pointerT]) {
+                pointerS++;
+            }
+            pointerT++;
+        }
+
+        return pointerS >= s.length();
+    }
+
+    public boolean isSubsequence1(String s, String t) {
+        int pointerS = 0;
+        int pointerT = 0;
+        while (pointerT < t.length() && pointerS < s.length()) {
+            if (s.charAt(pointerS) == t.charAt(pointerT)) {
+                pointerS++;
+            }
+            pointerT++;
+        }
+
+        return pointerS >= s.length();
+    }
+
+    public boolean isSubsequence2(String s, String t) {
+        if (s.length() == 0) {
+            return true;
+        }
+        int pos = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (s.charAt(pos) == t.charAt(i)) {
+                pos++;
+            }
+            if (pos == s.length()) {
+                return true;
+            }
+        }
+
+        return t.length() == pos;
+    }
+}
