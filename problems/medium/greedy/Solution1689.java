2e8f09087927956a68e06c9bb2057c501bcb9341
Dior <rustamovdior@gmail.com>
2023-06-17 12:42:06 +0200

    leetcode problem 1689

---
+++ problems/medium/greedy/Solution1689.java
@@ -0,0 +1,17 @@
+package problems.medium.greedy;
+
+/**
+ * Problem: <a href="https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers">
+ * partitioning-into-minimum-number-of-deci-binary-numbers</a>
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution1689 {
+    public int minPartitions(String n) {
+        int res = 0;
+        for (char c : n.toCharArray()) {
+            res = Math.max(res, c - '0');
+        }
+        return res;
+    }
+}
