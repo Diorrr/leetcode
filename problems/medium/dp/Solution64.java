a1b2429422717c464484c0f2f9c3b554f1b017ce
Dior <rustamovdior@gmail.com>
2023-06-20 09:00:22 +0200

    leetcode problem 64

---
+++ problems/medium/dp/Solution64.java
@@ -0,0 +1,29 @@
+package problems.medium.dp;
+
+/**
+ * Problem: <a href="https://leetcode.com/problems/minimum-path-sum">
+ * minimum-path-sum</a>
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution64 {
+
+    public int minPathSum(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < grid[0].length; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < grid.length; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[0].length; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+
+        return dp[grid.length - 1][grid[0].length - 1];
+    }
+}
