ab946be9a6a30d0ca5fa40fb0c3481b50e407b88
Dior <rustamovdior@gmail.com>
2023-07-30 11:48:49 +0200

    leetcode problem 50

---
+++ problems/medium/math/Solution50.java
@@ -0,0 +1,40 @@
+package problems.medium.math;
+
+
+/**
+ * Problem: <a href="https://leetcode.com/problems/powx-n">
+ * powx-n</a>
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution50 {
+    public double myPow(double x, int n) {
+        double add = 1;
+        if (n < 0) {
+            x = 1 / x;
+            if (n == Integer.MIN_VALUE) {
+                n = -Integer.MAX_VALUE;
+                add = x;
+            }
+            n = -n;
+        }
+
+        return add * calc(x, n);
+    }
+
+    public double calc(double x, int n) {
+        double val = 1;
+        if (n == 1) {
+            val = x;
+        } else if (n > 0 && n % 2 == 0) {
+            val = calc(x, n / 2);
+            val *= val;
+        } else if (n > 0) {
+            val = calc(x, n / 2);
+            val *= val;
+            val *= x;
+        }
+
+        return val;
+    }
+}
