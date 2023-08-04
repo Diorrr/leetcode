da359c57b366f89b7e5b0810f6f8c2794bb1209d
Dior <rustamovdior@gmail.com>
2023-08-04 21:42:13 +0200

    leetcode problem 2789

---
+++ problems/medium/greedy/Solution2789.java
@@ -0,0 +1,26 @@
+package problems.medium.greedy;
+
+/**
+ * Problem: <a href="https://leetcode.com/problems/largest-element-in-an-array-after-merge-operations">
+ * largest-element-in-an-array-after-merge-operations</a>
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution2789 {
+    public long maxArrayValue(int[] nums) {
+        long sum = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] <= sum) {
+                sum += nums[i];
+            } else {
+                sum = nums[i];
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
