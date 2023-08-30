d4215243fb818e7defc9ed5d91f3de88e69ee031
dior.rustamov <dior.rustamov@check24.de>
2023-08-30 13:14:11 +0200

    leetcode problem 242

---
+++ python/problems/easy/Solution242.py
@@ -0,0 +1,17 @@
+class Solution242:
+    def __init__(self):
+        pass
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = [0] * 26
+        for x in s:
+            count[ord(x)-ord('a')] += 1
+
+        for x in t:
+            count[ord(x)-ord('a')] -= 1
+
+        for val in count:
+            if val != 0:
+                return False
+
+        return True
