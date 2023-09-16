81265626b77412bd29a1c963c2c3361a9ef0dcbf
Dior <rustamovdior@gmail.com>
2023-09-16 17:18:36 +0200

    leetcode problem 181

---
+++ python/problems/easy/Solution181.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+    return employee.merge(employee, left_on='managerId', right_on='id').query("salary_x > salary_y")['name_x'].to_frame('Employee')
