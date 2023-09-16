9e81e1a1248e79f2fb003c9859b4fcf290d90e5d
Dior <rustamovdior@gmail.com>
2023-09-16 17:13:33 +0200

    leetcode problem 181

---
+++ sql/easy/Solution181.sql
@@ -0,0 +1,6 @@
+#
+Write your MySQL query statement below
+# https://leetcode.com/problems/employees-earning-more-than-their-managers
+SELECT name as 'Employee'
+FROM Employee e1
+WHERE e1.salary > (SELECT salary FROM Employee WHERE salary < e1.salary AND id = e1.managerId)
