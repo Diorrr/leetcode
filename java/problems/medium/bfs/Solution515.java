b3e8dc0d2cbab736a645b69bc3663390c2e11849
Dior <rustamovdior@gmail.com>
2023-10-24 16:12:39 +0200

    leetcode problem 515

---
+++ java/problems/medium/bfs/Solution515.java
@@ -0,0 +1,45 @@
+package problems.medium.bfs;
+
+
+import util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Problem: <a href="https://leetcode.com/problems/find-largest-value-in-each-tree-row">
+ * find-largest-value-in-each-tree-row</a>
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution515 {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        int rowMax = Integer.MIN_VALUE;
+        if (root != null) {
+            queue.add(root);
+        }
+        int count = queue.size();
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            rowMax = Math.max(rowMax, node.val);
+            count--;
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+            if (count == 0) {
+                res.add(rowMax);
+                rowMax = Integer.MIN_VALUE;
+                count = queue.size();
+            }
+        }
+        return res;
+    }
+}
