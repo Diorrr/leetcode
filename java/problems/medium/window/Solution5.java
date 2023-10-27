6cec21238ae2e7ddd65d5ff86dfa160fdf261ba6
Dior <rustamovdior@gmail.com>
2023-10-27 08:00:25 +0200

    leetcode problem 5

--- java/problems/medium/window/Solution5.java
+++ java/problems/medium/window/Solution5.java
@@ -1,14 +1,50 @@
 package problems.medium.window;
 
 /**
- * Problem: https://leetcode.com/problems/longest-palindromic-substring/
+ * Problem: <a href="https://leetcode.com/problems/longest-palindromic-substring">
+ * longest-palindromic-substring</a>
  * Time Complexity:
  * Space Complexity：
  */
 class Solution5 {
     public String longestPalindrome(String s) {
+        if (s.length() < 2) {
+            return s;
+        }
+        char[] chars = s.toCharArray();
+        String res = String.valueOf(chars[0]);
+        for (int i = 1; i < chars.length - 1; i++) {
+            int l = i;
+            int r = i;
+            while (l - 1 >= 0 && r + 1 < chars.length && chars[l - 1] == chars[r + 1]) {
+                l--;
+                r++;
+            }
+            if (r - l + 1 > res.length()) {
+                res = s.substring(l, r + 1);
+            }
+        }
+
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i - 1] == chars[i]) {
+                int l = i;
+                int r = i - 1;
+                while (l - 1 >= 0 && r + 1 < chars.length && chars[l - 1] == chars[r + 1]) {
+                    l--;
+                    r++;
+                }
+                if (r - l + 1 > res.length()) {
+                    res = s.substring(l, r + 1);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public String longestPalindrome2(String s) {
         for (int i = s.length(); i > 0; i--) {
-            for (int j = 0; j + i < s.length()+1; j++) {
+            for (int j = 0; j + i < s.length() + 1; j++) {
                 String substring = s.substring(j, j + i);
                 if (isPalindrome(substring)) {
                     return substring;
