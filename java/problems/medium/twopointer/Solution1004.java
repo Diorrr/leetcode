0bec18c2ff4c4d7f02c6fe7c82cdba5d58e95905
Dior <rustamovdior@gmail.com>
2023-11-04 12:00:50 +0100

    leetcode problem 1004

--- java/problems/medium/twopointer/Solution1004.java
+++ java/problems/medium/twopointer/Solution1004.java
@@ -8,21 +8,35 @@ package problems.medium.twopointer;
  */
 class Solution1004 {
     public int longestOnes(int[] nums, int k) {
+        int l = 0;
+        int r = 0;
+        for (r = 0; r < nums.length; r++) {
+            if (nums[r] == 0) {
+                k--;
+            }
+            if (k < 0 && nums[l++] == 0) {
+                k++;
+            }
+        }
+
+        return r - l;
+    }
+
+    public int longestOnes2(int[] nums, int k) {
         int res = 0;
-        int flip = k;
         int counter = 0;
         int l = 0, r = 0;
         while (l < nums.length && r < nums.length) {
             if (nums[r] == 1) {
                 counter++;
                 r++;
-            } else if (flip > 0) {
-                flip--;
+            } else if (k > 0) {
+                k--;
                 counter++;
                 r++;
             } else {
                 if (nums[l] == 0) {
-                    flip++;
+                    k++;
                 }
                 l++;
                 counter--;
