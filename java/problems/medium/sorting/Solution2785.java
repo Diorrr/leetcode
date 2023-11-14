0153cd4c2d959083ea90a698e5a1ffbb9456330d
dior.rustamov <dior.rustamov@check24.de>
2023-11-14 12:32:21 +0100

    leetcode problem 2785

--- java/problems/medium/sorting/Solution2785.java
+++ java/problems/medium/sorting/Solution2785.java
@@ -2,7 +2,7 @@ package problems.medium.sorting;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -15,6 +15,33 @@ import java.util.Set;
  */
 class Solution2785 {
     public String sortVowels(String s) {
+        char[] chars = s.toCharArray();
+        List<Integer> index = new ArrayList<>();
+        List<Character> vowels = List.of('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
+        int[] count = new int[vowels.size()];
+        HashMap<Character, Integer> vowelIndex = new HashMap<>();
+        for (int i = 0; i < vowels.size(); i++) {
+            vowelIndex.put(vowels.get(i), i);
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (vowelIndex.get(chars[i]) != null) {
+                index.add(i);
+                count[vowelIndex.get(chars[i])]++;
+            }
+        }
+        int pos = 0;
+        for (int i = 0; i < vowels.size(); i++) {
+            while (count[i] != 0) {
+                chars[index.get(pos)] = vowels.get(i);
+                pos++;
+                count[i]--;
+            }
+        }
+
+        return new String(chars);
+    }
+
+    public String sortVowels2(String s) {
         char[] chars = s.toCharArray();
         Set<Character> vowels = new HashSet<>(List.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
         List<Integer> index = new ArrayList<>();
