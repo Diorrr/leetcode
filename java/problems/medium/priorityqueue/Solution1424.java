9b046d5f731c737d5b1914335fe37968b02f8437
dior.rustamov <dior.rustamov@check24.de>
2023-11-22 13:23:36 +0100

    leetcode problem 1424

---
+++ java/problems/medium/priorityqueue/Solution1424.java
@@ -0,0 +1,34 @@
+package problems.medium.priorityqueue;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * Problem: <a href="https://leetcode.com/problems/diagonal-traverse-ii">
+ * diagonal-traverse-ii</a>
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution1424 {
+    public int[] findDiagonalOrder(List<List<Integer>> nums) {
+        List<Integer> res = new ArrayList<>();
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> {
+            if (a[1] + a[2] == b[1] + b[2]) {
+                return Integer.compare(b[1], a[1]);
+            }
+            return Integer.compare(a[1] + a[2], b[1] + b[2]);
+        });
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.get(i).size(); j++) {
+                queue.add(new int[]{nums.get(i).get(j), i, j});
+            }
+        }
+        while (!queue.isEmpty()) {
+            res.add(queue.poll()[0]);
+        }
+
+        return res.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
