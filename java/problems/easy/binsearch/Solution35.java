a35c1eb5cfe5d5408f674fc33e2503d5cd3166a0
Dior <rustamovdior@gmail.com>
2023-11-26 10:17:52 +0100

    leetcode problem 35

--- java/problems/easy/binsearch/Solution35.java
+++ java/problems/easy/binsearch/Solution35.java
@@ -1,12 +1,20 @@
-package problems.easy;
+package problems.easy.binsearch;
+
+import java.util.Arrays;
 
 /**
- * Problem: https://leetcode.com/problems/search-insert-position/
+ * Problem: <a href="https://leetcode.com/problems/search-insert-position">
+ * search-insert-position</a>
  * Time Complexity: O(logN)
- * Space Complexity： O(1)
+ * Space Complexity：O(1)
  */
 class Solution35 {
     public int searchInsert(int[] nums, int target) {
+        int pos = Arrays.binarySearch(nums, target);
+        return pos >= 0 ? pos : Math.abs(pos) - 1;
+    }
+
+    public int searchInsert2(int[] nums, int target) {
         int l = -1, r = nums.length;
         int m;
         while (l < r - 1) {
