6ce9793569633e68cd4dd8fd5b6a52ca57266cc5
Dior <rustamovdior@gmail.com>
2023-12-03 12:19:01 +0100

    leetcode problem 160

--- java/problems/easy/array/Solution283.java
+++ java/problems/easy/array/Solution283.java
@@ -5,27 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Problem: https://leetcode.com/problems/move-zeroes/
+ * Problem: <a href="https://leetcode.com/problems/move-zeroes">
+ * move-zeroes</a>
  * Time Complexity: O(NlogN) / O(N)
  * Space Complexity： O(N) / O(1)
  */
 class Solution283 {
     public void moveZeroes(int[] nums) {
-        List<Integer> res = Arrays.stream(nums).boxed().sorted((a, b) -> {
-            if (a == 0 && b != 0) {
-                return 1;
-            } else if (a != 0 && b == 0) {
-                return -1;
-            } else {
-                return 0;
-            }
-        }).collect(Collectors.toList());
-        for (int i = 0; i < res.size(); i++) {
-            nums[i] = res.get(i);
-        }
-    }
-
-    public void moveZeroes2(int[] nums) {
         int insertPos = 0;
         for (int num : nums) {
             if (num != 0) {
@@ -35,5 +21,42 @@ class Solution283 {
         for (int i = insertPos; i < nums.length; i++) {
             nums[i] = 0;
         }
+    }
+
+    public void moveZeroes1(int[] nums) {
+        List<Integer> res = Arrays.stream(nums).boxed().sorted((a, b) -> {
+            if (a == 0 && b != 0) {
+                return 1;
+            } else if (a != 0 && b == 0) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }).toList();
+        for (int i = 0; i < res.size(); i++) {
+            nums[i] = res.get(i);
+        }
+    }
+
+    public void moveZeroes2(int[] nums) {
+        System.arraycopy(
+                Arrays.stream(nums)
+                      .boxed()
+                      .sorted((a, b) -> {
+                          if (a == 0 && b != 0) {
+                              return 1;
+                          } else if (a != 0 && b == 0) {
+                              return -1;
+                          } else {
+                              return 0;
+                          }
+                      })
+                      .mapToInt(Integer::intValue)
+                      .toArray(),
+                0,
+                nums,
+                0,
+                nums.length
+        );
     }
 }
