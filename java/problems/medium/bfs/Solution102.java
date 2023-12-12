99829d1d708f74e9ff3c4b417ca3c2e0d2769c34
Dior <rustamovdior@gmail.com>
2023-12-12 09:46:09 +0100

    leetcode problem 102

--- java/problems/medium/bfs/Solution102.java
+++ java/problems/medium/bfs/Solution102.java
@@ -8,35 +8,65 @@ import java.util.List;
 import java.util.Queue;
 
 /**
- * Problem: https://leetcode.com/problems/binary-tree-level-order-traversal/description/
- * Time Complexity:
+ * Problem: <a href="https://leetcode.com/problems/binary-tree-level-order-traversal">
+ * binary-tree-level-order-traversal</a>
+ * Time Complexity: O(N)
  * Space Complexity：
  */
 class Solution102 {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
-        int level = 0;
         Queue<TreeNode> queue = new LinkedList<>();
-        if (root!=null) {
+        if (root != null) {
+            queue.add(root);
+            queue.add(null);
+        }
+
+        List<Integer> level = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            TreeNode curr = queue.poll();
+            if (curr == null) {
+                res.add(level);
+                level = new ArrayList<>();
+                if (!queue.isEmpty()) {
+                    queue.add(null);
+                }
+            } else {
+                level.add(curr.val);
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) {
             queue.add(root);
         }
 
         while (!queue.isEmpty()) {
-            res.add(new ArrayList<>());
             List<TreeNode> toAdd = new ArrayList<>();
+            List<Integer> toAddVal = new ArrayList<>();
             while (!queue.isEmpty()) {
                 TreeNode node = queue.poll();
-                if (node != null) {
-                    res.get(level).add(node.val);
-                    if (node.left != null) {
-                        toAdd.add(node.left);
-                    }
-                    if (node.right != null) {
-                        toAdd.add(node.right);
-                    }
+                toAddVal.add(node.val);
+                if (node.left != null) {
+                    toAdd.add(node.left);
                 }
+                if (node.right != null) {
+                    toAdd.add(node.right);
+                }
+
             }
-            level++;
+            res.add(toAddVal);
             queue.addAll(toAdd);
         }
 
