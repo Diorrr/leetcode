c609596dc242b7bcfee732696b67d90b8c4bca92
Dior <rustamovdior@gmail.com>
2023-12-17 11:21:47 +0100

    leetcode problem 5

--- java/problems/medium/window/Solution5.java
+++ java/problems/medium/window/Solution5.java
@@ -42,6 +42,33 @@ class Solution5 {
         return res;
     }
 
+    // dp solution from comments
+    public String longestPalindrome1(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int startRes = 0;
+        int resLength = 1;
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int end = 0; end < n; end++) {
+            for (int start = end - 1; start >= 0; start--) {
+                if (chars[start] == chars[end] && (end - start == 1 || dp[start + 1][end - 1])) {
+                    dp[start][end] = true;
+                    if (end - start + 1 > resLength) {
+                        startRes = start;
+                        resLength = end - start + 1;
+                    }
+                }
+            }
+        }
+
+        return s.substring(startRes, startRes + resLength);
+    }
+
     public String longestPalindrome2(String s) {
         for (int i = s.length(); i > 0; i--) {
             for (int j = 0; j + i < s.length() + 1; j++) {
@@ -56,13 +83,15 @@ class Solution5 {
     }
 
     private boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
+        int left = 0;
+        int right = s.length() - 1;
         while (left <= right) {
             if (s.charAt(left) != s.charAt(right))
                 return false;
             left++;
             right--;
         }
+
         return true;
     }
 }
