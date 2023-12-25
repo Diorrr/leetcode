f6cf7157cbd5bafbed5ef347f4ff1880fb851216
Dior <rustamovdior@gmail.com>
2023-12-25 10:56:39 +0100

    leetcode problem 207

--- java/problems/medium/bfs/Solution207.java
+++ java/problems/medium/bfs/Solution207.java
@@ -6,44 +6,87 @@ import java.util.List;
 import java.util.Queue;
 
 /**
- * Problem: https://leetcode.com/problems/course-schedule/
- * Time Complexity:
- * Space Complexity：
+ * Problem: <a href="https://leetcode.com/problems/course-schedule">
+ * course-schedule</a>
+ * Time Complexity: O(V + E)
+ * Space Complexity： O(V)
  */
 class Solution207 {
+    ArrayList<Integer>[] graph;
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int counter = 0;
-        List<List<Integer>> graph = new ArrayList<>(numCourses);
+        graph = new ArrayList[numCourses];
+        boolean[] used = new boolean[numCourses];
+        boolean[] recStack = new boolean[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int[] prerequisite : prerequisites) {
+            graph[prerequisite[1]].add(prerequisite[0]);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (isCycle(i, used, recStack)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isCycle(int node, boolean[] used, boolean[] recStack) {
+        if (recStack[node]) {
+            return true;
+        }
+        if (used[node]) {
+            return false;
+        }
+        used[node] = true;
+        recStack[node] = true;
+        for (Integer to : graph[node]) {
+            if (isCycle(to, used, recStack)) {
+                return true;
+            }
+        }
+        recStack[node] = false;
+
+        return false;
+    }
+
+    public boolean canFinish1(int numCourses, int[][] prerequisites) {
+        List<Integer>[] graph = new List[numCourses];
         int[] degree = new int[numCourses];
         Queue<Integer> queue = new LinkedList<>();
 
-        for (int i = 0; i < numCourses; i++) {
-            graph.add(i, new ArrayList<>());
-        }
         for (int[] prerequisite : prerequisites) {
-            degree[prerequisite[1]]++;
-            graph.get(prerequisite[0]).add(prerequisite[1]);
+            if (graph[prerequisite[1]] == null) {
+                graph[prerequisite[1]] = new ArrayList<>();
+            }
+            degree[prerequisite[0]]++;
+            graph[prerequisite[1]].add(prerequisite[0]);
         }
 
         for (int i = 0; i < degree.length; i++) {
             if (degree[i] == 0) {
                 queue.add(i);
-                counter++;
+                numCourses--;
             }
         }
 
         while (!queue.isEmpty()) {
             int from = queue.poll();
-            for (int i = 0; i < graph.get(from).size(); i++) {
-                int to = graph.get(from).get(i);
-                degree[to]--;
-                if (degree[to] == 0) {
-                    counter++;
-                    queue.add(to);
+            if (graph[from] != null) {
+                for (Integer to : graph[from]) {
+                    degree[to]--;
+                    if (degree[to] == 0) {
+                        numCourses--;
+                        queue.add(to);
+                    }
                 }
             }
         }
 
-        return counter == numCourses;
+        return numCourses == 0;
     }
 }
--- java/problems/medium/dfs/Solution200.java
+++ java/problems/medium/dfs/Solution200.java
@@ -10,64 +10,8 @@ import java.util.Queue;
  * Space Complexity： O(1)
  */
 class Solution200 {
-    int[] dir = {-1, 0, 0, -1, 0, 1, 1, 0};
-
-    public class UnionFind {
-        private int[] parent;
-        int count;
-
-        public UnionFind(char[][] grid) {
-            parent = new int[grid.length * grid[0].length];
-            count = 0;
-            for (int i = 0; i < grid.length; i++) {
-                for (int j = 0; j < grid[0].length; j++) {
-                    if (grid[i][j] == '1') {
-                        parent[i * grid[0].length + j] = i * grid[0].length + j;
-                        count++;
-                    }
-                }
-            }
-        }
-
-        public int find(int i) {
-            if (parent[i] == i) {
-                return i;
-            }
-            parent[i] = find(parent[i]);
-            return parent[i];
-        }
-
-        public void union(int i, int j) {
-            int irep = find(i);
-            int jrep = find(j);
-
-            if (irep != jrep) {
-                parent[irep] = jrep;
-                count--;
-            }
-        }
-    }
-
-    public int numIslands(char[][] grid) {
-        UnionFind unionFind = new UnionFind(grid);
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    for (int k = 0; k < dir.length; k += 2) {
-                        int newX = i + dir[k], newY = j + dir[k + 1];
-                        if (newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length) {
-                            unionFind.union(i * grid[0].length + j, (i + dir[k]) * grid[0].length + j + dir[k + 1]);
-                        }
-                    }
-                }
-            }
-        }
-
-        return unionFind.count;
-    }
-
     // sol from comments
-    public int numIslands1(char[][] grid) {
+    public int numIslands(char[][] grid) {
         int res = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
@@ -97,6 +41,63 @@ class Solution200 {
         }
 
     }
+
+    int[] dir = {-1, 0, 0, -1, 0, 1, 1, 0};
+    public class UnionFind {
+
+        private int[] parent;
+
+        int count;
+
+        public UnionFind(char[][] grid) {
+            parent = new int[grid.length * grid[0].length];
+            count = 0;
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (grid[i][j] == '1') {
+                        parent[i * grid[0].length + j] = i * grid[0].length + j;
+                        count++;
+                    }
+                }
+            }
+        }
+        public int find(int i) {
+            if (parent[i] == i) {
+                return i;
+            }
+            parent[i] = find(parent[i]);
+            return parent[i];
+        }
+
+        public void union(int i, int j) {
+            int irep = find(i);
+            int jrep = find(j);
+
+            if (irep != jrep) {
+                parent[irep] = jrep;
+                count--;
+            }
+        }
+
+    }
+    public int numIslands1(char[][] grid) {
+        UnionFind unionFind = new UnionFind(grid);
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    for (int k = 0; k < dir.length; k += 2) {
+                        int newX = i + dir[k], newY = j + dir[k + 1];
+                        if (newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length) {
+                            unionFind.union(i * grid[0].length + j, (i + dir[k]) * grid[0].length + j + dir[k + 1]);
+                        }
+                    }
+                }
+            }
+        }
+
+        return unionFind.count;
+    }
+
 
     public int numIslands2(char[][] grid) {
         int res = 0;
