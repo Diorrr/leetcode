fafa406f5320df4ad8b2758d558b924fc7488108
Dior <rustamovdior@gmail.com>
2023-12-28 11:31:54 +0100

    leetcode problem 49

--- java/problems/medium/string/Solution49.java
+++ java/problems/medium/string/Solution49.java
@@ -1,12 +1,7 @@
 package problems.medium.string;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.*;
 
 /**
  * Problem: <a href="https://leetcode.com/problems/group-anagrams">
@@ -16,6 +11,38 @@ import java.util.stream.Collectors;
  */
 class Solution49 {
     public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String str : strs) {
+            String letterCode = getLetterCode(str);
+            if (map.containsKey(letterCode)) {
+                res.get(map.get(letterCode)).add(str);
+            } else {
+                map.put(letterCode, res.size());
+                res.add(new ArrayList<>());
+                res.get(res.size() - 1).add(str);
+            }
+        }
+
+        return res;
+    }
+
+    public List<List<String>> groupAnagrams1(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String letterCode = new String(chars);
+            map.putIfAbsent(letterCode, new ArrayList<>());
+            map.get(letterCode).add(str);
+        }
+
+        return map.values().stream().toList();
+    }
+
+    public List<List<String>> groupAnagrams2(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
 
         for (String str : strs) {
@@ -28,13 +55,11 @@ class Solution49 {
     }
 
     private String getLetterCode(String str) {
-        int[] letterCount = new int[26];
+        char[] letterCount = new char[26];
         for (char c : str.toCharArray()) {
             letterCount[c - 'a']++;
         }
 
-        return Arrays.stream(letterCount)
-                     .mapToObj(t-> t +",")
-                     .collect(Collectors.joining());
+        return new String(letterCount);
     }
 }
