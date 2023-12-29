ba51d78f3ff4525225f010e8d2e40b6afa8f5b2c
Dior <rustamovdior@gmail.com>
2023-12-29 10:43:40 +0100

    leetcode problem 763

---
+++ java/problems/medium/greedy/Solution763.java
@@ -0,0 +1,34 @@
+package problems.medium.greedy;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Problem: <a href="https://leetcode.com/problems/partition-labels">
+ * partition-labels</a>
+ * Time Complexity: O(N)
+ * Space Complexity： O(N)
+ */
+class Solution763 {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> res = new ArrayList<>();
+        int[] pos = new int[26];
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            pos[chars[i] - 'a'] = i;
+        }
+        int start = 0;
+        int maxPos = pos[chars[0] - 'a'];
+        for (int i = 0; i < chars.length; i++) {
+            if (i == maxPos) {
+                res.add(maxPos - start + 1);
+                start = i + 1;
+                maxPos = i + 1 < chars.length ? pos[chars[start] - 'a'] : -1;
+            }
+            maxPos = Math.max(pos[chars[i] - 'a'], maxPos);
+        }
+
+        return res;
+    }
+}
