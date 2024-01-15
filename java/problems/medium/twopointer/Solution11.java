3226cd6eaea736c7bd82030dc3833618b074485a
Dior <rustamovdior@gmail.com>
2024-01-15 12:15:50 +0100

    leetcode problem 11

--- java/problems/medium/twopointer/Solution11.java
+++ java/problems/medium/twopointer/Solution11.java
@@ -1,14 +1,39 @@
 package problems.medium.twopointer;
 
 /**
- * Problem: https://leetcode.com/problems/container-with-most-water/
+ * Problem: <a href="https://leetcode.com/problems/container-with-most-water">
+ * container-with-most-water</a>
  * Time Complexity: O(N)
  * Space Complexity：O(1)
  */
 class Solution11 {
+
     public int maxArea(int[] height) {
         int res = 0;
-        int l = 0, r = height.length - 1;
+        int l = 0;
+        int r = height.length - 1;
+        int length = height.length;
+        int currentArea;
+        while (length-- != 0) {
+            if (height[l] <= height[r]) {
+                currentArea = length * height[l];
+                l++;
+            } else {
+                currentArea = length * height[r];
+                r--;
+            }
+            if (currentArea > res) {
+                res = currentArea;
+            }
+        }
+
+        return res;
+    }
+
+    public int maxArea2(int[] height) {
+        int res = 0;
+        int l = 0;
+        int r = height.length - 1;
         while (l < r) {
             res = Math.max(res, (r - l) * Math.min(height[l], height[r]));
             if (height[l] < height[r]) {
