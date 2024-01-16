695f63d7c51308c2f65130ff0c9cedf02ffe41af
Dior <rustamovdior@gmail.com>
2024-01-16 11:55:14 +0100

    leetcode problem 15

--- java/problems/medium/twopointer/Solution15.java
+++ java/problems/medium/twopointer/Solution15.java
@@ -3,22 +3,33 @@ package problems.medium.twopointer;
 import java.util.*;
 
 /**
- * Problem: https://leetcode.com/problems/3sum/
- * Time Complexity:
+ * Problem: <a href="https://leetcode.com/problems/3sum">
+ * 3sum</a>
+ * Time Complexity: O(N^2)
  * Space Complexity：
  */
 class Solution15 {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> res = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            int l = i + 1, r = nums.length - 1;
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            int l = i + 1;
+            int r = nums.length - 1;
             while (l < r) {
                 int sum = nums[i] + nums[l] + nums[r];
                 if (sum == 0) {
                     res.add(List.of(nums[i], nums[l], nums[r]));
                     l++;
                     r--;
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                    while (l < r && nums[r + 1] == nums[r]) {
+                        r--;
+                    }
                 } else if (sum < 0) {
                     l++;
                 } else {
@@ -27,7 +38,6 @@ class Solution15 {
             }
         }
 
-
-        return new ArrayList<>(res);
+        return res;
     }
 }
