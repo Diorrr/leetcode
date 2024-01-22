312b6235c60187e1974caba708c1db1411c3ae00
Dior <rustamovdior@gmail.com>
2024-01-22 17:00:42 +0100

    leetcode problem 32

--- java/problems/hard/string/Solution32.java
+++ java/problems/hard/string/Solution32.java
@@ -8,6 +8,25 @@ package problems.hard.string;
  */
 class Solution32 {
     public int longestValidParentheses(String s) {
+        int[] longestValid = new int[s.length()];
+        int res = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    longestValid[i] = 2 + (i > 1 ? longestValid[i - 2] : 0);
+                } else if (i - longestValid[i - 1] - 1 >= 0 && s.charAt(i - longestValid[i - 1] - 1) == '(') {
+                    longestValid[i] = 2 + longestValid[i - 1] +
+                            ((i - longestValid[i - 1] - 2 >= 0) ? longestValid[i - longestValid[i - 1] - 2] : 0);
+
+                }
+                res = Math.max(longestValid[i], res);
+            }
+        }
+
+        return res;
+    }
+
+    public int longestValidParentheses2(String s) {
         char[] chars = s.toCharArray();
         int res = 0;
         for (int i = 0; i < chars.length; i++) {
