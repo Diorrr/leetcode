c5ac03732c575fcb320cad7fddd99c5bf693e883
dior.rustamov <dior.rustamov@check24.de>
2024-01-25 12:06:35 +0100

    leetcode problem 1143

--- java/problems/medium/dp/Solution1143.java
+++ java/problems/medium/dp/Solution1143.java
@@ -10,11 +10,12 @@ package problems.medium.dp;
 class Solution1143 {
     public int longestCommonSubsequence(String text1, String text2) {
         int[][] dp = new int[text1.length() + 1][text2.length() + 1];
-
-        for (int i = 0; i < text1.length(); i++) {
-            for (int j = 0; j < text2.length(); j++) {
+        char[] chars1 = text1.toCharArray();
+        char[] chars2 = text2.toCharArray();
+        for (int i = 0; i < chars1.length; i++) {
+            for (int j = 0; j < chars2.length; j++) {
                 dp[i + 1][j + 1] =
-                        text1.charAt(i) == text2.charAt(j) ? dp[i][j] + 1 : Math.max(dp[i][j + 1], dp[i + 1][j]);
+                        chars1[i] == chars2[j] ? dp[i][j] + 1 : Math.max(dp[i][j + 1], dp[i + 1][j]);
             }
         }
 
