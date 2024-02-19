bb1d50e55c88357508f956817608d6cef4ca7b2f
Dior <rustamovdior@gmail.com>
2024-02-19 08:05:02 +0100

    leetcode problem 231

--- java/problems/easy/Solution231.java
+++ java/problems/easy/Solution231.java
@@ -1,12 +1,34 @@
 package problems.easy;
 
 /**
- * Problem: https://leetcode.com/problems/power-of-two/
- * Time Complexity:
- * Space Complexity：
+ * Problem: <a href="https://leetcode.com/problems/power-of-two">
+ * power-of-two</a>
+ * Time Complexity: O(log(N))
+ * Space Complexity： O(1)
  */
 class Solution231 {
     public boolean isPowerOfTwo(int n) {
+        if (n == 0 || n < 0) {
+            return false;
+        }
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isPowerOfTwo1(int n) {
+        if (n == 0 || n < 0) {
+            return false;
+        }
+        return Math.pow(2, (int) (Math.log(n) / Math.log(2))) == n;
+    }
+
+    public boolean isPowerOfTwo2(int n) {
         int i = 0;
         while (n >= (1L << i)) {
             if (n == (1L << i)) {
