c86a633e3cb88ab4fd8038ff3df610322e4a7e49
Dior <rustamovdior@gmail.com>
2024-02-20 12:46:59 +0100

    leetcode problem 268

--- java/problems/easy/Solution268.java
+++ java/problems/easy/Solution268.java
@@ -3,16 +3,23 @@ package problems.easy;
 import java.util.Arrays;
 
 /**
- * Problem: https://leetcode.com/problems/missing-number/
- * Time Complexity: O(N*logN)
- * Space Complexity：
+ * Problem: <a href="https://leetcode.com/problems/missing-number">
+ * missing-number</a>
+ * Time Complexity: O(N)
+ * Space Complexity： O(1)
  */
 class Solution268 {
     public int missingNumber(int[] nums) {
+        int res = nums.length * (nums.length + 1) / 2;
+        for (int num : nums) {
+            res -= num;
+        }
+        return res;
+    }
+
+    public int missingNumber2(int[] nums) {
         Arrays.sort(nums);
-        if (nums[0] != 0) {
-            return 0;
-        } else if (nums[nums.length - 1] == nums.length - 1) {
+        if (nums[nums.length - 1] == nums.length - 1) {
             return nums.length;
         } else {
             for (int i = 0; i < nums.length; i++) {
