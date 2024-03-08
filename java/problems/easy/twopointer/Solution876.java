972fe33d7326de920b40dc7652d014b46a621eed
dior.rustamov <dior.rustamov@check24.de>
2024-03-08 13:09:50 +0100

    leetcode problem 3005

--- java/problems/easy/twopointer/Solution876.java
+++ java/problems/easy/twopointer/Solution876.java
@@ -4,10 +4,10 @@ import util.ListNode;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 /**
- * Problem: https://leetcode.com/problems/middle-of-the-linked-list/
+ * Problem: <a href="https://leetcode.com/problems/middle-of-the-linked-list">
+ * middle-of-the-linked-list</a>
  * Time Complexity: O(N)
  * Space Complexity： (N)
  */
@@ -23,23 +23,5 @@ class Solution876 {
         }
 
         return list.get(list.size() / 2);
-    }
-
-    public ListNode middleNode2(ListNode head) {
-        int count;
-        if (Objects.nonNull(head)) {
-            count = 1;
-        } else {
-            return head;
-        }
-        ListNode next = head.next;
-        while (Objects.nonNull(next)) {
-            count++;
-            next = next.next;
-        }
-        for (int i = 0; i < count / 2; i++) {
-            head = head.next;
-        }
-        return head;
     }
 }
