622cd8b9518b2a689e6e87dd36b4730bad56d0b1
Dior <rustamovdior@gmail.com>
2024-03-25 11:20:40 +0100

    leetcode problem 1356

--- java/problems/medium/arrays/Solution442.java
+++ java/problems/medium/arrays/Solution442.java
@@ -4,12 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Problem: https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
- * Time Complexity:
- * Space Complexity：
+ * Problem: <a href="https://leetcode.com/problems/find-all-duplicates-in-an-array">
+ * find-all-duplicates-in-an-array</a>
+ * Time Complexity: O(N)
+ * Space Complexity： O(N)
  */
 class Solution442 {
     public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+
+        for (int num : nums) {
+            int idx = Math.abs(num) - 1;
+            if (nums[idx] < 0) {
+                res.add(idx + 1);
+            }
+            nums[idx] = -nums[idx];
+        }
+
+        return res;
+    }
+
+    public List<Integer> findDuplicates2(int[] nums) {
         int[] used = new int[nums.length + 1];
         List<Integer> res = new ArrayList<>();
 
