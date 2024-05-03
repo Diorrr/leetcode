a5db0b3c5f261b6466ba924eb38b677140622aa5
Dior <rustamovdior@gmail.com>
2024-05-03 06:54:40 +0200

    leetcode problem 165

--- java/problems/medium/dp/Solution2370.java
+++ java/problems/medium/dp/Solution2370.java
@@ -15,10 +15,8 @@ class Solution2370 {
         Map<Character, Integer> charMax = new HashMap<>();
         int dp[] = new int[s.length()];
         int res = 1;
-        dp[0] = 1;
-        charMax.put(s.charAt(0), 1);
 
-        for (int i = 1; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             char currChar = s.charAt(i);
             for (char neigh = (char) Math.max('a', currChar - k); neigh <= Math.min('z', currChar + k); neigh++) {
                 dp[i] = Math.max(dp[i], charMax.getOrDefault(neigh, -1) + 1);
@@ -28,11 +26,5 @@ class Solution2370 {
         }
 
         return res;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(
-                new Solution2370().longestIdealString("a", 4)
-        );
     }
 }
