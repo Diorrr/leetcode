b27c171f306257b2151ff6ae3418b8d6d91dff73
Dior <rustamovdior@gmail.com>
2024-08-18 14:46:31 +0200

    leetcode problem 264

---
+++ java/problems/medium/string/Solution1190.java
@@ -0,0 +1,39 @@
+package problems.medium.string;
+
+
+import java.util.Stack;
+
+/**
+ * Problem: <a href="https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses">
+ * reverse-substrings-between-each-pair-of-parentheses</a>
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution1190 {
+    public String reverseParentheses(String s) {
+        StringBuilder res = new StringBuilder();
+        char[] word = s.toCharArray();
+        Stack<Integer> open = new Stack<>();
+        for (int i = 0; i < word.length; i++) {
+            if (word[i] == '(') {
+                open.add(i);
+            } else if (word[i] == ')') {
+                int l = open.pop();
+                int r = i;
+                while (l < r) {
+                    char temp = word[l];
+                    word[l] = word[r];
+                    word[r] = temp;
+                    l++;
+                    r--;
+                }
+            }
+        }
+        for (char c : word) {
+            if (c != '(' && c != ')') {
+                res.append(c);
+            }
+        }
+        return res.toString();
+    }
+}
