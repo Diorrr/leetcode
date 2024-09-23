b47b4e348ebeb8b91d962e69a5e572b3d16b77be
dior.rustamov <dior.rustamov@check24.de>
2024-09-23 12:47:40 +0200

    leetcode problem 884

--- java/problems/medium/dp/Solution2707.java
+++ java/problems/medium/dp/Solution2707.java
@@ -1,7 +1,10 @@
 package problems.medium.dp;
 
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Problem: <a href="https://leetcode.com/problems/extra-characters-in-a-string">
@@ -12,6 +15,22 @@ import java.util.HashMap;
 class Solution2707 {
     public int minExtraChar(String s, String[] dictionary) {
         int[] dp = new int[s.length() + 1];
+        Set<String> set = new HashSet<>(Arrays.asList(dictionary));
+
+        for (int i = 1; i <= s.length(); i++) {
+            dp[i] = dp[i - 1] + 1;
+            for (int j = 0; j < i; j++) {
+                String tempR = s.substring(j, i);
+                if (set.contains(tempR)) {
+                    dp[i] = Math.min(dp[i], dp[j]);
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public int minExtraChar2(String s, String[] dictionary) {
+        int[] dp = new int[s.length() + 1];
         HashMap<String, Integer> dictMap = new HashMap<>();
         for (String word : dictionary) {
             dictMap.put(word, 0);
@@ -21,7 +40,6 @@ class Solution2707 {
         for (int i = 1; i <= s.length(); i++) {
             dp[i] = i;
             for (int j = 0; j < i; j++) {
-                String tempL = s.substring(0, j);
                 String tempR = s.substring(j, i);
                 dp[i] = Math.min(dp[i], dp[j] + dictMap.getOrDefault(tempR, tempR.length()));
             }
