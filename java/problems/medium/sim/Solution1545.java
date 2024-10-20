9052bf29c046d9d3c6b9251d2ff7ce2b40969302
Dior <rustamovdior@gmail.com>
2024-10-20 12:05:48 +0200

    leetcode problem 1106

---
+++ java/problems/medium/sim/Solution1545.java
@@ -0,0 +1,32 @@
+package problems.medium.sim;
+
+
+/**
+ * Problem: <a href="https://leetcode.com/problems/find-kth-bit-in-nth-binary-string">
+ * find-kth-bit-in-nth-binary-string</a>
+ * Time Complexity:
+ * Space Complexity：
+ */
+class Solution1545 {
+    public char findKthBit(int n, int k) {
+        boolean curr = false;
+        int len = 1;
+        for (int i = 0; i < n; i++) {
+            len = len * 2 + 1;
+        }
+        for (int i = 1; i < n; i++) {
+            if ((len + 1) / 2 == k) {
+                return curr ? '0' : '1';
+            } else if ((len + 1) / 2 < k) {
+                k = len - k + 1;
+                curr = !curr;
+            }
+            len = (len - 1) / 2;
+        }
+        char res = k == 1 ? '0' : '1';
+        if (curr) {
+            res = res == '0' ? '1' : '0';
+        }
+        return res;
+    }
+}
