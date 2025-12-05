63d6da87b501db5a4fdbed1fc6f628b056c0a80f
dior.rustamov <dior.rustamov@check24.de>
2025-12-05 09:33:09 +0100

    advent of code, day 5

---
+++ java/problems/aoc25/SolutionDay5.java
@@ -0,0 +1,85 @@
+package problems.aoc25;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Problem: <a href="https://adventofcode.com/2025/day/5">
+ * day5</a>
+ */
+class SolutionDay5 {
+    void solve2() throws FileNotFoundException {
+        BigInteger res = BigInteger.ZERO;
+        List<BigInteger[]> ranges = new ArrayList<>();
+        List<BigInteger[]> mergedRanges = new ArrayList<>();
+        String fileName = "java/problems/aoc25/input/dat5.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            // read line by line until EOF (null)
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("-");
+                BigInteger start = new BigInteger(parts[0].trim());
+                BigInteger end = new BigInteger(parts[1].trim());
+                ranges.add(new BigInteger[]{start, end});
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ranges.sort(Comparator.comparing(a -> a[0]));
+        BigInteger[] current = ranges.get(0);
+        mergedRanges.add(current);
+        for (BigInteger[] range : ranges) {
+            if (range[0].compareTo(current[1])<1){
+                current[1]= current[1].max(range[1]);
+            } else {
+                current = range;
+                mergedRanges.add(current);
+            }
+        }
+        for (BigInteger[] mergedRange : mergedRanges) {
+            res = res.add(mergedRange[1].subtract(mergedRange[0]).add(BigInteger.ONE));
+        }
+        System.out.println(res);
+    }
+
+    void solve1() throws FileNotFoundException {
+        int res = 0;
+        List<BigInteger[]> range = new ArrayList<>();
+        String fileName = "java/problems/aoc25/input/dat5.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            // read line by line until EOF (null)
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("-")) {
+                    String[] parts = line.split("-");
+                    BigInteger start = new BigInteger(parts[0].trim());
+                    BigInteger end = new BigInteger(parts[1].trim());
+                    range.add(new BigInteger[]{start, end});
+                } else if (!line.isEmpty()) {
+                    BigInteger num = new BigInteger(line);
+                    if (isAvailable(num, range)) {
+                        res++;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(res);
+    }
+
+    private boolean isAvailable(BigInteger num, List<BigInteger[]> range) {
+        for (BigInteger[] ints : range) {
+            if (num.compareTo(ints[0]) > -1 && num.compareTo(ints[1]) < 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
